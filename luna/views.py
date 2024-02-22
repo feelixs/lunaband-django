36f34b3489da83d3022c560a5a5d98dff9338a89
Michael Felix <mikemh@uri.edu>
2024-02-22 11:23:36 -0500

    Update views.py

--- luna/views.py
+++ luna/views.py
@@ -3,3 +3,7 @@ from asgiref.sync import sync_to_async
 from luna.settings import SITE_IP, SITE_HOST, STATIC_ROOT
 import os
 import aiofiles
+
+
+async def index(request):
+    return render(request, 'index.html')
