eb50dec4e69e60e4695aee5f78444da9c9e64662
Michael Felix <mikemh@uri.edu>
2024-02-27 10:29:01 -0500

    Update data.js

--- luna/static/js/data.js
+++ luna/static/js/data.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+
+var btn = document.getElementById('ajaxbtn');
+
+btn.addEventListener('click'), makereq, false);
+
+
+function makereq() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            document.getElementById('main-contents').innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.open('GET', 'https://trioluna.com/static/data/data.html', true);
+    xhr.send(null);
+}
