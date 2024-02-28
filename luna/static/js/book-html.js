44774ea0870b67d0de9f19816632304361b58a92
Michael Felix <mikemh@uri.edu>
2024-02-28 12:40:48 -0500

    Update book-html.js

--- luna/static/js/book-html.js
+++ luna/static/js/book-html.js
@@ -1,6 +1,7 @@
-console.log('hello');
-
-var $imgs = $('img').css('opacity', '0.5');
+function allOpacity(val) {
+    var $imgs = $('img')
+    $imgs.css('opacity', 0.5);
+}
 
 
 function loadHTML(url) {
@@ -13,3 +14,10 @@ function loadHTML(url) {
     req.open('GET', url, true);
     req.send(null);
 }
+
+var $don = $('#don-quixote-img');
+$don.on('click', function() {
+    loadHTML('https://trioluna.com/static/data/cervantes-data.html');
+    allOpacity(0.5);
+    $don.css('opacity', 1);
+});
