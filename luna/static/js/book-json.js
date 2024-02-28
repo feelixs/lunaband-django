d5c24bad351ffbf3d8b85831f0fef847c2500c71
Michael Felix <mikemh@uri.edu>
2024-02-28 13:35:00 -0500

    Update book-json.js

--- luna/static/js/book-json.js
+++ luna/static/js/book-json.js
@@ -1,3 +1,26 @@
+var $don = $('#don-quixote-img');
+var $tale = $('#two-cities-img');
+var $lord = $('#lotr-img');
+
+$don.on('click', function() {
+    loadJSON('http://trioluna.com/static/data/book-data.json', 0);
+    allOpacity(0.5);
+    $don.css('opacity', 1);
+});
+
+$don.on('click', function() {
+    loadJSON('http://trioluna.com/static/data/book-data.json', 1);
+    allOpacity(0.5);
+    $tale.css('opacity', 1);
+});
+
+$don.on('click', function() {
+    loadJSON('http://trioluna.com/static/data/book-data.json', 2);
+    allOpacity(0.5);
+    $lord.css('opacity', 1);
+});
+
+
 function allOpacity(val) {
     var $imgs = $('img')
     $imgs.css('opacity', 0.5);
@@ -22,27 +45,3 @@ function loadJSON(url, index) {
     req.open('GET', url, true);
     req.send(null);
 }
-
-
-var $don = $('#don-quixote-img');
-$don.on('click', function() {
-    loadJSON('http://trioluna.com/static/data/book-data.json', 0);
-    allOpacity(0.5);
-    $don.css('opacity', 1);
-});
-
-var $tale = $('#two-cities-img');
-$don.on('click', function() {
-    loadJSON('http://trioluna.com/static/data/book-data.json', 1);
-    allOpacity(0.5);
-    $tale.css('opacity', 1);
-});
-
-
-var $lord = $('#lotr-img');
-$don.on('click', function() {
-    loadJSON('http://trioluna.com/static/data/book-data.json', 2);
-    allOpacity(0.5);
-    $lord.css('opacity', 1);
-});
-
