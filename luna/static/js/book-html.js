ccbe4d5f2b760ccc431404f0075562bef4602e33
Michael Felix <mikemh@uri.edu>
2024-02-28 12:35:41 -0500

    Update book-html.js

--- luna/static/js/book-html.js
+++ luna/static/js/book-html.js
@@ -1,3 +1,15 @@
 console.log('hello');
 
 var $imgs = $('img').css('opacity', '0.5');
+
+
+function loadHTML(url) {
+    var req = new XMLHttpRequest();
+    req.onload = function() {
+        if (req.status === 200) {
+            $('#details').html(req.responseText);
+        }
+    };
+    req.open('GET', url, true);
+    req.send(null);
+}
