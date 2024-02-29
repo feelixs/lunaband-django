12ad01ee595e499daf83b16b56cd83341922c798
Michael Felix <mikemh@uri.edu>
2024-02-28 19:41:45 -0500

    Update gallery.js

--- luna/static/js/gallery.js
+++ luna/static/js/gallery.js
@@ -1,17 +1,16 @@
 var galTextFields = []
 var galDualImages = []
 
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function () {
     /*
         Load this page's content in the default language when the page loads.
     */
-    galTextFields.push(new DualLangTextField(`/static/text/footer`, document.getElementById('footer-text')));
-    galTextFields.push(new DualLangTextField( `/static/text/copyright`, document.getElementById('copyright')));
+    galTextFields.push(new DualLangTextField(`/static/text/footer`, $('#footer-text')));
+    galTextFields.push(new DualLangTextField( `/static/text/copyright`, $('#copyright')));
     /* when we fetch the text from the server, we need a fullpath that included 'luna'.
      when we set the image div in a duallangimage, we don't need 'luna' */
     galDualImages.push(new DualLangImage('http://trioluna.com/static/images/buttons/globe-white-en.webp',
-                                            'http://trioluna.com/static/images/buttons/globe-white-es.webp',
-        document.getElementById('change-language-img')))
+        'http://trioluna.com/static/images/buttons/globe-white-es.webp', $('#change-language-img')))
     loadContentInLang(currentLang);
     loadGallery();
 })
@@ -20,9 +19,9 @@ function applyMainLanguageChange(newlang) {
     /*
         Applies language change to the page's main text, this will be overriden for each page
     */
-
+    var $galleryTitle = $('#page-title');
     document.title = newlang === 'es' ? 'Luna | Medios' : 'Luna | Media';
-    document.getElementById('page-title').innerText = newlang === 'es' ? 'Galería' : 'Gallery';
+    $galleryTitle.html(newlang === 'es' ? 'Galería' : 'Gallery');
     loadContentInLang(newlang)
 }
 
@@ -39,25 +38,21 @@ function loadContentInLang(language) {
 }
 
 function loadGallery() {
-    document.getElementById('gallery-placeholder').remove(); // remove the placeholder "loading gallery" text
+    $('#gallery-placeholder').remove(); // remove the placeholder "loading gallery" text
 
-    let pictures = [ '068A62F9.jpeg', 'P1001442.JPG',  '468D91AF.jpeg', 'P1001211.webp',
-                             'P1001437.jpg', 'P1001384.jpg', 'P1001197.jpg', 'P1001430.jpg',
-                             'P1001262.webp', 'P1001396.jpg', 'P1001427.jpg', 'P1001195.jpg',
-                             'P1000427.jpg', 'P1000708.jpg', 'P1001406.webp', 'P1001446.webp' ]
+    var pictures = [ '068A62F9.webp', 'P1001442.webp',  '468D91AF.webp', 'P1001211.webp',
+                             'P1001437.webp', 'P1001384.webp', 'P1001197.webp', 'P1001430.webp',
+                             'P1001262.webp', 'P1001396.webp', 'P1001427.webp', 'P1001195.webp',
+                             'P1000427.webp', 'P1000708.webp', 'P1001406.webp', 'P1001446.webp' ]
 
-    let galleryDiv = document.getElementById('main-gallery-container');
-    for (let i = 0; i < pictures.length; i++) {
-        let imgDiv = document.createElement('div');
-        imgDiv.className = 'gallery-container';
-        let img = document.createElement('img');
-        img.className = 'gallery-img rounded';
-        img.src = `http://trioluna.com/static/images/gallery/imgs/${pictures[i]}`;
-        setAltToFile(img, `${trimFilename(pictures[i])}.txt`); // dynamically det the img's alt
-
-        imgDiv.appendChild(img); // put the image inside its div
-        galleryDiv.appendChild(imgDiv); // put the div inside the gallery
-    }
+    var $galleryDiv = $('#main-gallery-container');
+    pictures.forEach(function (picFilename) {
+        let $tempDiv = $('<div>').attr('class', 'gallery-container');
+        let $tempImg = $('<img>').attr('class', 'gallery-img rounded').attr('src', `images/gallery/imgs/${picFilename}`);
+        $tempDiv.append($tempImg);
+        $galleryDiv.append($tempDiv);
+        setAltToFile($tempImg, `${trimFilename(picFilename)}.txt`);
+    })
 }
 
 function trimFilename(filename) {
