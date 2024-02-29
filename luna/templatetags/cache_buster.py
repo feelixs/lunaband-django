39d791b3461956cf2fdfe7ffe8998a83b8f5c7d1
Michael Felix <mikemh@uri.edu>
2024-02-28 20:52:42 -0500

    Update cache_buster.py

--- luna/templatetags/cache_buster.py
+++ luna/templatetags/cache_buster.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.simple_tag
 def cache_buster(static_file):
     """Appends the edit timestamp to the static file so the browser always refreshes its cache when the file's been edited"""
-    full_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "../felixcreations-web/static", static_file)
+    full_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "luna/static", static_file)
     url_path = f"/static/{static_file}"
     try:
         timestamp = os.path.getmtime(full_path) # file modification time
