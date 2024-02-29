6c41550d2e693b058b3455e0223eb96c99cc517e
Michael Felix <mikemh@uri.edu>
2024-02-28 19:51:08 -0500

    webps

--- luna/static/images/gallery/imgs/gen.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# List all files in the current directory
-files = os.listdir('..')
-
-# Format each file name as a list item in Python syntax
-file_list_syntax = [f"{file}" for file in files]
-
-print(file_list_syntax)
