0c24baae2b2700b1924671216fef49a0e46516e8
Michael Felix <mikemh@uri.edu>
2024-03-02 14:13:05 -0500

    fix server endpoint

--- luna/endpoints/api.py
+++ luna/endpoints/api.py
@@ -1,8 +1,15 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from luna import views
+from django.views.decorators.csrf import csrf_exempt
+from asgiref.sync import sync_to_async, async_to_sync
 
 @require_http_methods(["POST"])
+
+
+@sync_to_async
+@csrf_exempt
+@async_to_sync
 async def set_language(request):
     # endpoint to set the user's default language - uses the 'request' object to set the user's session cookie
     # can be called thru javascript
