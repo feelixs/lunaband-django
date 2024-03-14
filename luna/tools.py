8de10e26823c2ca0e01b29364cf175009c0ab65f
Michael Felix <mikemh@uri.edu>
2024-03-14 12:39:01 -0400

    Create tools.py

---
+++ luna/tools.py
@@ -0,0 +1,17 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+
+async def send_email(me: str, to: str, subject: str, message: str):
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    # me == the sender's email address
+    # family = the list of all recipients' email addresses
+    msg['From'] = me
+    msg['To'] = to
+    msg.preamble = message
+
+    # Send the email via our own SMTP server.
+    s = smtplib.SMTP('localhost')
+    s.sendmail(me, to, msg.as_string())
+    s.quit()
