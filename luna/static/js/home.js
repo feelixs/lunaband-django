38c4f2a2276ecd49dba70a46585083eb73752c73
Michael Felix <mikemh@uri.edu>
2024-03-17 12:07:17 -0400

    Update home.js

--- luna/static/js/home.js
+++ luna/static/js/home.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
     /*
         Load this page's content in the default language when the page loads.
     */
-    homeTextFields.push(new XMLDualLangTextField(`/static/data/xml/home.xml`, 0, $('#div1-text'), '&ensp;&ensp;&ensp;&ensp;'));
-    homeTextFields.push(new XMLDualLangTextField(`/static/data/xml/home.xml`, 1, $('#div2-text'), '&ensp;&ensp;&ensp;&ensp;'));
-    homeTextFields.push(new XMLDualLangTextField(`/static/data/xml/home.xml`, 2, $('#patreon-text'), '&ensp;&ensp;&ensp;&ensp;'));
+    homeTextFields.push(new XMLDualLangTextField(`/static/data/xml/home.xml`, 0, $('#div1-text')));
+    homeTextFields.push(new XMLDualLangTextField(`/static/data/xml/home.xml`, 1, $('#div2-text')));
+    homeTextFields.push(new XMLDualLangTextField(`/static/data/xml/home.xml`, 2, $('#patreon-text')));
     homeTextFields.push(new HTMLDualLangTextField(`/static/data/html/footer`, $('#footer-text')));
     homeTextFields.push(new HTMLDualLangTextField( `/static/data/html/copyright`, $('#copyright')));
     homeDualImages.push(new DualLangImage('https://trioluna.com/static/images/buttons/globe-white-en.webp',
