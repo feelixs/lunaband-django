2fc96098510dcc3f400165493e4cf01b4ed0599e
Michael Felix <mikemh@uri.edu>
2024-04-02 10:19:53 -0400

    Update urls.py

--- luna/urls.py
+++ luna/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import handler404
 from django.urls import path
 from luna import views
 from luna.endpoints import api
