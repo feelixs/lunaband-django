bf274f1276007284144b688ac866c7a80beacb2c
Michael Felix <mikemh@uri.edu>
2024-04-09 11:15:43 -0400

    Update api.py

--- luna/endpoints/api.py
+++ luna/endpoints/api.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 import traceback
 from django.views.decorators.http import require_http_methods
 from luna import views
+import json
 from django.views.decorators.csrf import csrf_exempt
 from asgiref.sync import sync_to_async, async_to_sync
 
@@ -12,7 +13,7 @@ async def set_language_api(request):
     # endpoint to set the user's default language - uses the 'request' object to set the user's session cookie
     # can be called thru javascript
     try:
-        data = await request.json()
+        data = json.loads(request.body)
         language = data.get('language')
         if not language:
             return JsonResponse({'error': 'Language parameter is missing'}, status=400)
