9c973f0594753146d307a8c8b89454312f52dda8
Michael Felix <mikemh@uri.edu>
2024-04-09 11:00:22 -0400

    Update views.py

--- luna/views.py
+++ luna/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from asgiref.sync import sync_to_async
 from luna.settings import SITE_IP, SITE_HOST, STATIC_ROOT
 import os
 
 
-async def set_language(request, language):
-    await sync_to_async(request.session.__setitem__)('language', language)
+def set_language(request, language):
+    language = request.GET.get('language', 'en')  # Default to English if not specified
+    response = HttpResponse(f"Language set to '{language}'")
+    response.set_cookie('language', language, max_age=60 * 60 * 24 * 365)  # 1 year
+    return response
 
 def handler404(request, exception=None, template_name='errors/404.html'):
     return render(request, template_name, status=404)
@@ -14,13 +17,13 @@ def handler500(request):
     return render(request, 'errors/500.html')
 
 async def get_language(request):
-    language = await sync_to_async(request.session.get)('language')  # get language from user data
+    language = await sync_to_async(request.COOKIES.get)('language', 'en')  # get language from user data
     if language in ['es', 'en']:
         return language
 
     # no valid language set
     default = os.getenv('DEFAULT_LANGUAGE')
-    await set_language(request, default)
+    await sync_to_async(set_language)(request, default)
     return default
 
 
