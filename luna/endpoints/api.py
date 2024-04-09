b8c2f0780b79610ba062b30a32bccfc4b6918391
Michael Felix <mikemh@uri.edu>
2024-04-09 11:10:22 -0400

    Update api.py

--- luna/endpoints/api.py
+++ luna/endpoints/api.py
@@ -19,4 +19,4 @@ async def set_language_api(request):
         await sync_to_async(views.set_language)(request, language)
         return JsonResponse({'message': 'Language set successfully'}, status=200)
     except:
-        return render(request, 'errors/err.html', {"e": traceback.format_exc()}, status=500)
+        return JsonResponse({'message': traceback.format_exc()})
